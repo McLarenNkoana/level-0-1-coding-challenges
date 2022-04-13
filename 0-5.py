c06a1437209654de73420672785c8df945d9c3bb
McLaren Nkoana <mclarennkoana@gmail.com>
2022-04-13 22:38:54 +0200

    modified 0-5

--- 0-5.py
+++ 0-5.py
@@ -1,5 +1,7 @@
 def area(num1, num2, num3):
-    s = 1/2 * (num1 + num2 + num3)
-    return s
+    semi = 1/2 * (num1 + num2 + num3)
+    triangle = (semi * (semi - num1) * (semi - num2) * (semi - num3)) ** 0.5
+
+    return triangle
 
 print(area(3, 4, 5))
