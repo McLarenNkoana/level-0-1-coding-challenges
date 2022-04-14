80140d754935537164ebec1fb279dd621a75d786
McLaren Nkoana <mclarennkoana@gmail.com>
2022-04-14 12:09:42 +0200

    modified 0-7

--- 0-7.py
+++ 0-7.py
@@ -1,14 +1,14 @@
 def fahrenheit(temp1):
-    f = (temp1 * 1.8) + 32
-    return f
+    fahrenheit_conversion = (temp1 * 1.8) + 32
+    return fahrenheit_conversion
 
 
 print(fahrenheit(80))
 
 
 def celsius(temp2):
-    c = (temp2 - 32) * 5/9
-    return c
+    celsius_conversion = (temp2 - 32) * 5/9
+    return celsius_conversion
 
 
 print(celsius(80))
