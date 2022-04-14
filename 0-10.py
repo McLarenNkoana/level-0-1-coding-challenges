e6e2e117ed44cf41ef6747242a860e406ecd7ce3
McLaren Nkoana <mclarennkoana@gmail.com>
2022-04-14 16:25:59 +0200

    modified 0-10

--- 0-10.py
+++ 0-10.py
@@ -1,7 +1,7 @@
 def common(input1, input2):
     word1 = set(input1)
     word2 = set(input2)
-    characters = tuple(word1 & word2)
+    characters = (", ").join(word1 & word2)
     print("Common letters: {}".format(characters))
 
 common("house", "computers")
