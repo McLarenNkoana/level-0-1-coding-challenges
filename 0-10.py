121b9226e1abfb661de574f9d14dac2fdc3650e7
McLaren Nkoana <mclarennkoana@gmail.com>
2022-04-14 13:07:20 +0200

    modified 0-10

--- 0-10.py
+++ 0-10.py
@@ -1,7 +1,10 @@
 def common(input1, input2):
     word1 = set(input1)
     word2 = set(input2)
-    characters = list(word1 & word2)
+    characters = tuple(word1 & word2)
     print("Common letters: {}".format(characters))
 
 common("house", "computers")
+
+# txt = "For only {price:.2f} dollars!"
+# print(txt.format(price = 49))
