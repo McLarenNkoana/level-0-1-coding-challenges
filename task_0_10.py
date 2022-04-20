e70df3e1356e8759a413c569d9824fdd95a6381e
McLaren Nkoana <89813734+McLarenNkoana@users.noreply.github.com>
2022-04-20 13:11:37 +0200

    Update task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -1,7 +1,8 @@
 def common(input1, input2):
-    word1 = set(input1)
-    word2 = set(input2)
+    word1 = set(input1.lower())
+    word2 = set(input2.lower())
     characters = (", ").join(word1 & word2)
+
     print("Common letters: {}".format(characters))
 
-common("house", "computers")
+common("house", "cOmputers")
