d51df742918dd5e3bad326c948509cd22587d2d5
McLaren Nkoana <89813734+McLarenNkoana@users.noreply.github.com>
2022-04-22 22:24:26 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -2,14 +2,13 @@ def time(number):
     mins = number%60
     hours = number//60
 
-    if hours > 1:
+    if hours > 1 or hours == 0:
         print(hours,"hours,", end = ' ')
     else:
         print(hours, "hour,", end = ' ')
-    if mins > 1:
+    if mins > 1 or mins == 0:
         print(mins, "minutes")
     else:
         print(mins, "minute")
 
-time(71)
-
+time(0)
